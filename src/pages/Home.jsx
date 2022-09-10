45e6b51fb5e399fa6631f17cb3d41eda031ca035
AmanRana10 <amanranathegr8@gmail.com>
2022-09-11 00:49:47 +0530

    created preview cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,78 @@
 import React, { useState } from 'react';
+import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
 
+  let isShowsSearch = searchOption === 'shows';
   const onInputChange = event => {
     setInput(event.target.value);
   };
 
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(response => response.json())
-      .then(res => {
-        console.log(res);
-      });
+    apiGet(`/search/${searchOption}?q=${input}`).then(res => {
+      setResults(res);
+      console.log(res);
+    });
   };
 
-  const onEnter = (ev) => {
-    if(ev.keyCode === 13)
-      onSearch();
-  }
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
+  const onEnter = ev => {
+    if (ev.keyCode === 13) onSearch();
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    }
+    if (results && results.length > 0) {
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
+    }
+
+    return null;
+  };
   return (
     <MainPageLayout>
       <input type="text" onChange={onInputChange} onKeyDown={onEnter} />
+      <div>
+        <label htmlFor="search-shows">
+          Shows
+          <input
+            id="search-shows"
+            type="radio"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+        <label htmlFor="search-actors">
+          Actors
+          <input
+            id="search-actors"
+            type="radio"
+            value="people"
+            checked={!isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
       <button type="button" onClick={onSearch}>
         Search
       </button>
+
+      {renderResults()}
     </MainPageLayout>
   );
 };
